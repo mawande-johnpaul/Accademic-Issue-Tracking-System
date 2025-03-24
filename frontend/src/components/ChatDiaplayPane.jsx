dfafe7c57977091b44486cf138144daefa63593d
Paulecongu <econgupaul9@gmail.com>
2025-03-24 15:27:20 +0300

    Resolved some errors in frontend

--- frontend/src/components/ChatDiaplayPane.jsx
+++ frontend/src/components/ChatDiaplayPane.jsx
@@ -1,13 +1,14 @@
 import React from 'react'; 
 
 const ChatDisplayPane = ({heading, items}) => {
+    console.log(items)
     return (
         <div className='display-pane'>
             <div className='pane-header'>{heading}</div>
             <div className='pane-content'>
             {
-            items.map(item => (
-                <div key={item.id}>
+            items.map((item, i) => (
+                <div key={i}>
                     <div className='message-item'> 
                         <div className='message-header'>{item.name}</div>
                         <div className='message-body'>{item.message}</div>
