72830e4222ce0477cf74ef2d2673233d30c3f0a1
Mawande John Paul <mawandejohnpaul130@gmail.com>
2025-04-03 16:53:11 +0300

    Update settings.py

--- backend/AITS/settings.py
+++ backend/AITS/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'logbook',
+    'AITS.logbook',
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
