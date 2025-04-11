a68579b19308fb16081d86034fe86ed463fdc16e
prechardmichelle <prechardmichelle@gmail.com>
2025-04-11 07:29:58 -0700

    fixed css conflicts

--- frontend/src/components/table.jsx
+++ frontend/src/components/table.jsx
@@ -5,7 +5,8 @@ const Table = () => {
         <table>
             <tr className="headings">
                 <th className="admin">Admin</th>
-                <th className="name">Name</th>                  
+                <th className="name">Name</th> 
+                <th className="admin">Student</th>                 
             </tr>
             <tr>
                 <td></td>
