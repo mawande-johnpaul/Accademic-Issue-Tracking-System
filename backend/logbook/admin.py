22e6bb8f10de6895fdba0e8337959cd7a35af75c
angelnamuli <angelnamuli@icloud.com>
2025-04-12 17:34:56 +0300

    Update admin.py
    add a custom header for our django admin page

--- backend/logbook/admin.py
+++ backend/logbook/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 
 #class CustomAdmin(admin.ModelAdmin):
     #list_display = ('name', 'department')
@@ -7,6 +8,12 @@ from .models import *
     #list_filter = ('department')
 
 # Register your models here.
-admin.site.register(CustomUser)
+# admin.site.register(CustomUser)
 admin.site.register(Issue)
+admin.site.site_header = "Academic Issue Tracking System"
+
 #admin.site.register(CustomUser, CustomAdmin)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
