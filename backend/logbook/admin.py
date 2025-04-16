8db4056c1ca17fa85ddb8472065c909307331722
angelnamuli <angelnamuli@icloud.com>
2025-04-16 13:13:49 +0300

    Update admin.py
     admin to create users logic and registers the admin as well

--- backend/logbook/admin.py
+++ backend/logbook/admin.py
@@ -13,3 +13,7 @@ admin.site.site_header = "Academic Issue Tracking System"
 admin.site.site_title = "AITS Admin"
 admin.site.index_title = "Welcome to AITS Panel"
 #admin.site.register(CustomUser, CustomAdmin)
+#admin can create users
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
