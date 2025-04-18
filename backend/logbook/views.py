cbe6fec7c2a8c4ece95b6904c1bdb7ce13ca66c5
angelnamuli <angelnamuli@icloud.com>
2025-04-18 03:22:14 +0300

    Update views.py
    add logic for the changepasswordView

--- backend/logbook/views.py
+++ backend/logbook/views.py
@@ -220,3 +220,21 @@ from django.dispatch import receiver
         if created:
             EmailView.send_email('New Issue', 'A new issue has been created', User'''
 
+class ChangePasswordAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        current_password = request.data.get("current_password")
+        new_password = request.data.get("new_password")
+
+        if not current_password or not new_password:
+            return Response({'error': 'Both fields are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not user.check_password(current_password):
+            return Response({'error': 'Incorrect current password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+
+        return Response({'success': 'Password updated successfully'}, status=status.HTTP_200_OK)
