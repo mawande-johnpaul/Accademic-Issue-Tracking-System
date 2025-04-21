08f18cadc1ca8497fdfe2b04cc715d1f460f5dc5
angelnamuli <angelnamuli@icloud.com>
2025-04-22 01:09:27 +0300

    update settings.py
    catering for number of requests made by user and the limitations

--- backend/AITS/settings.py
+++ backend/AITS/settings.py
@@ -170,5 +170,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ]
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+        "rest_framework.throttling.AnonRateThrottle",
     ],
 }
