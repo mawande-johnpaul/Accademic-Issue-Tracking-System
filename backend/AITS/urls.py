b49cb141d3b8e90712af847ce8fe7c7aac879338
angelnamuli <angelnamuli@icloud.com>
2025-05-13 15:37:57 +0300

    Update urls.py
    add path for the change username view

--- backend/AITS/urls.py
+++ backend/AITS/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', home_view, name='home'),  # Root URL
     path('home/', home_view, name='home'),  # Optional, in case you want /home/ to work too
     path("account/update-recovery/", UpdateRecoveryInfoView.as_view(), name="update_recovery"),
+    path("api/settings/change-username/",ChangeUsernameAPIView.as_view(),name='api_change_username'),
 
 
 
