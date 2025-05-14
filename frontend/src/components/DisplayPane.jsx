b14453dfeafd00f43a4d0e630dca4b945f697ed6
angelnamuli <angelnamuli@icloud.com>
2025-05-14 17:27:47 +0300

    Update DisplayPane.jsx

--- frontend/src/components/DisplayPane.jsx
+++ frontend/src/components/DisplayPane.jsx
@@ -1,23 +1,45 @@
-import React from 'react'; 
+// import React from 'react'; 
 
-const DisplayPane = ({heading, items}) => {
+// const DisplayPane = ({heading, items}) => {
+//     return (
+//         <div className='display-pane'>
+//             <div className='pane-header'>{heading}</div>
+//             <div className='pane-content'>
+//             {
+//             items.map(item => (
+//                 <div key={item.id}>
+//                     <div className='message-item'> 
+//                         <div className='message-header'>{item.name}</div>
+//                         <div className='message-body'>{item.message}</div>
+//                     </div>
+//                 </div>
+//             ))
+//             }
+//             </div>
+//         </div>
+//     )
+// };
+
+// export default DisplayPane;
+import React from 'react';
+
+const DisplayPane = ({ items }) => {
     return (
-        <div className='display-pane'>
-            <div className='pane-header'>{heading}</div>
-            <div className='pane-content'>
-            {
-            items.map(item => (
-                <div key={item.id}>
-                    <div className='message-item'> 
-                        <div className='message-header'>{item.name}</div>
-                        <div className='message-body'>{item.message}</div>
-                    </div>
-                </div>
-            ))
-            }
+        <div className="pane">
+            <div className="pane-header">
+                Notifications
+            </div>
+            <div className="pane-content">
+                {
+                    items.map((item1, index) => (
+                        <div key={index} className="notification-item">
+                            {item1}
+                        </div>
+                    ))
+                }
             </div>
         </div>
-    )
+    );
 };
 
 export default DisplayPane;
