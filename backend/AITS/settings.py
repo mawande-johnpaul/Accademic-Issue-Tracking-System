8c8590938facb54a5520a4ad2384f4ae04992c90
Mawande John PAul <johnpauljones181103@gmail.com>
2025-05-14 10:33:07 +0300

    fixed deployment

--- backend/AITS/settings.py
+++ backend/AITS/settings.py
@@ -142,11 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-'''STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, 'frontend', 'dist'),
-]'''
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
