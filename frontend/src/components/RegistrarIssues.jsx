4f8c87a661dc6f0e6eb13ed97c877c6f5c0c83d2
angelnamuli <angelnamuli@icloud.com>
2025-05-14 17:27:44 +0300

    Create RegistrarIssues.jsx

---
+++ frontend/src/components/RegistrarIssues.jsx
@@ -0,0 +1,26 @@
+import IssueCard from "./IssueCard";
+
+const RegistrarIssues = ({issues, type, token, content, setContent, setid}) => {
+  return (
+    <div className="max-w-2xl mx-auto mt-6">
+      {type === 'new' ?(
+        <>
+          {issues.map((issue) => (
+            <IssueCard key = {issue.id} isssue={issue} type={type} token={token} content={content} setContent={setContent} setid={setid}/>
+          ))}
+        </>
+      ) : type === 'assigned' ?(
+        <>
+          {issues.map((issue) => (
+            <IssueCard key = {issue.id} isssue={issue} type={type} token={token} content={content} setContent={setContent} setid={setid}/>
+          ))}        
+        </>
+      ): (
+        <></>
+      )}
+
+    </div>
+  );
+};
+
+export default RegistrarIssues;
