34c0f54cee4035d22e24481e7239b5200ea65ad6
angelnamuli <angelnamuli@icloud.com>
2025-05-14 23:14:46 +0300

    Update urls.py
    add relevant documentation

--- backend/AITS/urls.py
+++ backend/AITS/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 
     
     
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) # handles the url for submitting media files
 
 handler404 = 'logbook.views.custom_404_redirect'
 if settings.DEBUG:
