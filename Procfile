e954965865143aa9fbafe8f5f8b5f31f4052395a
Mawande John PAul <johnpauljones181103@gmail.com>
2025-05-14 13:58:51 +0300

    Removed buggy configuration

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn AITS.wsgi:application --bind 0.0.0.0:$PORT
--- frontend/src/components/InPageLoginButton.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-const InPageLoginButton = () => {
-    return (
-        <Link className="InPageLoginButton" to="/">Login/Signup</Link>
-    );
-}
-
-export default InPageLoginButton;
