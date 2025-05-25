91b18f8cdceb7c67c9998e1d46bf053e6e2cf29e
angelnamuli <angelnamuli@icloud.com>
2025-05-25 21:50:59 +0300

    Update signals.py

--- backend/logbook/signals.py
+++ backend/logbook/signals.py
@@ -4,13 +4,15 @@ from .models import Issue, Notification
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
+# Signal receiver to notify user when the status of their issue changes
 @receiver(pre_save, sender=Issue)
 def issue_status_change_notify(sender, instance, **kwargs):
+    # Only proceed if the instance already exists i.e., is being updated
     if not instance.pk:
         return
 
     try:
+        # Fetch the current state of the issue from the database
         old_instance = Issue.objects.get(pk=instance.pk)
     except Issue.DoesNotExist:
         return
